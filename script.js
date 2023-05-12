e289b2f1b6fce3f174d936f25bdab90d9ad2fa2f
Amey <130839577+ameynagulwar@users.noreply.github.com>
2023-05-12 10:29:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
 function threeSum(arr, target) {
 // write your code here
+	arr.sort((a, b) => a - b);
+	let closetSum = -100;
+
+	for(let i = 0; i < arr.length-2; i++){
+		let left = i + 1;
+		let right = arr.length - 1;
+
+		while(left < right){
+			const sum = arr[i] + arr[left] + arr[right];
+			 if (Math.abs(target - sum) < Math.abs(target - closestSum)) {
+             closestSum = sum;
+		}
+		
+      if (sum > target){
+        right--;
+      } 
+	  else {
+        left++;
+      }
+			
+	}
   
+}
+	return closetSum;
 }
 
 module.exports = threeSum;
