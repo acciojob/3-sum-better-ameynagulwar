dccbd2cf2dd683a9f53f9b2a3ae983a52b7b9941
Amey <130839577+ameynagulwar@users.noreply.github.com>
2023-05-12 10:41:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function threeSum(arr, target) {
 // write your code here
 	arr.sort((a, b) => a - b);
-	let closetSum = Infinity;
+	let closetSum = 0;
 
 	for(let i = 0; i < arr.length-2; i++){
 		let left = i + 1;
